62ef3e1a22731e97bbac69a51e046f5b9fae0cf5
Mario Germán Reyes C <mager19@gmail.com>
2023-07-07 15:58:41 -0500

    services block progress

--- blocks/register-blocks.php
+++ blocks/register-blocks.php
@@ -2,3 +2,4 @@
 register_block_type(get_template_directory() . '/blocks/title-textRight/block.json');
 register_block_type(get_template_directory() . '/blocks/intro-page/block.json');
 register_block_type(get_template_directory() . '/blocks/row/block.json');
+register_block_type(get_template_directory() . '/blocks/servicesBlock/block.json');
---
+++ blocks/servicesBlock/block.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Block Name
+ */
+
+//Creamos un id unico para nuestro block
+$block_id = 'services-block-' . $block['id'];
+if (!empty($block['anchor'])) {
+    $block_id = $block['anchor'];
+}
+
+//Creamos una clase para nuestro bloque y agregamos las clases añadidas en el editor
+$block_class = 'servicesBlock';
+if (!empty($block['className'])) {
+    $block_class .= ' ' . $block['className'];
+}
+
+//Fields
+$titleSection = get_field('title_services_block');
+$services = get_field('services');
+
+
+?>
+
+<div id="<?php echo $block_id ?>" class="<?php echo $block_class ?>">
+    <div class="container mx-auto">
+        <div class="flex flex-wrap px-4">
+            <?php if ($titleSection) { ?>
+                <div class="w-full">
+                    <h3 class="text-title3"><?php echo $titleSection; ?></h3>
+                </div>
+            <?php
+            } ?>
+
+            <?php if ($services) { ?>
+                <div class="services__container grid grid-cols-1 gap-8 md:grid-cols-3">
+                    <?php
+                    foreach ($services as $service) { ?>
+                        <div class="item__service">
+                            <h5 class="text-display1 text-primary">
+                                <?php echo $service['number']; ?>
+                            </h5>
+                            <h4 class="text-title6">
+                                <?php echo $service['title_service']; ?>
+                            </h4>
+                            <p><?php echo $service['description_service']; ?></p>
+
+
+                            <?php if ($service['link_service']) { ?>
+
+                                <a class="text-primary" href="<?php echo $service['link_service']['url']; ?>">
+                                    <?php echo $service['link_service']['title']; ?>
+                                </a>
+
+                            <?php
+                            } ?>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            <?php
+
+            } ?>
+        </div>
+    </div>
+</div>
