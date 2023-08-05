ca5631b22d62be7335a61f772440f4e7e91e1a5f
Mario Germán Reyes C <mager19@gmail.com>
2023-08-05 14:28:53 -0500

    row sizes added

--- blocks/row/block.php
+++ blocks/row/block.php
@@ -16,12 +16,22 @@ if (!empty($block['className'])) {
     $block_class .= ' ' . $block['className'];
 }
 
+$size = get_field('size');
+
+if ($size == 'mid') {
+    $sizeClass = 'w-full lg:w-8/12';
+} else {
+    $sizeClass = 'w-full';
+}
+
 ?>
 
 <div id="<?php echo $block_id ?>" class="<?php echo $block_class ?> py-8 lg:py-14">
     <div class="container mx-auto">
         <div class="flex flex-wrap px-4 [&_ul]:ml-5">
-            <InnerBlocks />
+            <div class="<?php echo $sizeClass; ?> mx-auto">
+                <InnerBlocks />
+            </div>
         </div>
     </div>
 </div>
